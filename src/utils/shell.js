9b5bf94f4f23ce51864f691b3177f5e605237030
chailu <chailu@rp-pet.cn>
2021-08-13 16:05:54 +0800

    fix:添加命令

--- src/utils/shell.js
+++ src/utils/shell.js
@@ -1,6 +1,11 @@
 
 const shell = require("shelljs");  // 执行文件操作
 const argv = require('yargs').argv; // yargs 处理参数
-const commit = argv._[0];
+const commit = argv._[0] || "update";
 
 console.log(commit)
+
+shell.exec('git add .')
+shell.exec(`git commit -m "${commit}"`)
+shell.exec(`git pull`)
+shell.exec('git push')
