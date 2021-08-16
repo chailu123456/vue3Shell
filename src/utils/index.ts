1262f9697da438d93629ac9b6f4656527b28ac75
chailu <chailu@rp-pet.cn>
2021-08-16 15:05:02 +0800

    添加打印颜色

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,15 +1,15 @@
 
+import colors from "colors";
 import shell from "shelljs";
 const commit = process.argv.splice(2);
 let describe = "update";
 if(commit.length) {
   describe = commit.join('')
 }
-console.log(describe)
 
 shell.exec('git add .')
 shell.exec(`git commit -m "${describe}"`)
 shell.exec(`git pull`)
-console.log('正在推送')
+console.log(colors.green('正在推送'));
 shell.exec('git push')
-console.log('推送成功')
+console.log(colors.green('推送成功'));
