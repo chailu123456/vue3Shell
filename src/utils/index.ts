6a2c5f1c3ed3c85f5232cdec3de6d74bd9de007e
chailu <chailu@rp-pet.cn>
2021-08-23 17:47:11 +0800

    build

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -11,50 +11,35 @@ const run = async () => {
   let buildParmas = null;
   if(describe.indexOf('build') != -1 ) {
     console.log(colors.green('项目打包中，请稍等片刻~~~'));
-
     buildParmas = new Promise((resolve,reject) => {
-      console.log(2)
-      const status = shell.exec(`npm run build`); // 打包失败不走catch
-      if(status.indexOf('failed')) {
+      const {code} = shell.exec(`npm run viteBuild`); // 打包失败不走catch
+      console.log(code)
+      // code = 0 成功
+      if(code) {
         reject('打包失败')
       } else {
         resolve('打包成功')
       }
     })
-
-
-    // try {
-    //   await new Promise((resolve,reject) => {
-    //     shell.exec(`npm run build`)
-    //     console.log(colors.green('打包成功'));
-    //   }).then(res => {
-
-    //   }).catch(err => {
-
-    //   })
-    // } catch(error) {
-    //   console.log(222)
-    //   console.log(colors.red(`打包失败${error.message}`));
-    //   process.exit(1)
-    // }
   }
  
-
-  const { stdout } = shell.exec('git symbolic-ref --short -q HEAD'); // 获取当前分支
+  // 获取当前分支
+  const { stdout } = shell.exec('git symbolic-ref --short -q HEAD');
   currentBranch = stdout;
   console.log(colors.green(`当前分支为${currentBranch}`))
   
+  // 代码推送
   const pushCode = () => {
     shell.exec('git add .');
     shell.exec(`git commit -m "${describe}"`);
     try {
       console.log(colors.green(`尝试推送分支 ${currentBranch} 至远程仓库`));
-      const { code } = shell.exec(`git push origin u`);
-      console.log(code)
+      // code = 0 成功
+      const { code } = shell.exec(`git push origin ${currentBranch}`);
       if(!code) {
         console.log(colors.green(`${currentBranch} 分支推送成功`));
       } else {
-        throw "为空";
+        throw "";
       }
     } catch(error) {
       console.log(colors.red(`推送分支失败: ${error}`))
@@ -64,23 +49,13 @@ const run = async () => {
   if (buildParmas) {
     buildParmas.then(res => {
       console.log(colors.green('打包成功'));
-      pushCode()
+      pushCode();
     }).catch(err => {
       console.log(colors.red(err));
     })
   } else {
-
     pushCode()
   }
-  // try {
-  //    const { stdout } = await shell.exec('git symbolic-ref --short -q HEAD'); // 获取当前分支
-  //    currentBranch = stdout;
-  //    console.log(colors.green(`当前分支为${currentBranch}`))
-  // } catch(error) {
-  //   console.log(colors.red(`获取分支失败: ${error.message}`))
-  //   process.exit(1) // 以失败码退出，用于 git hooks 拦截识别
-  // }
-  
 
 }
 
