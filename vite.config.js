1d8126a014d554c60689232ae92bd77c5a78dbfd
“youe <846304126@qq.com>
2021-08-30 23:21:30 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  configurations: [
+    {
+      "type": "pwa-chrome",
+      "request": "launch",
+      "name": "Launch Chrome against localhost",
+      "url": "http://localhost:8080",
+      "webRoot": "${workspaceFolder}"
+    }
+  ]
+  
 })
