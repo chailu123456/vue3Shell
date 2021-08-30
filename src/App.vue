140b39b806f1384ac5a0f084448967afbebf76b8
chailu <chailu@rp-pet.cn>
2021-08-30 17:15:16 +0800

    ttt

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="使用shell命令进行操作git,远程推送代码"/>
   <HelloWorld msg="使用shell命令进行操作git,远程推送代码"/>
+  <HelloWorld msg="使用shell命令进行操作git,远程推送代码"/>
+  <HelloWorld msg="使用shell命令进行操作git,远程推送代码"/>
 
 </template>
 
