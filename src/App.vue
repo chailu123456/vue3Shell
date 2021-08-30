457bdc7ab9f2c6b487cbba767916889948964b8b
chailu <chailu@rp-pet.cn>
2021-08-30 17:05:52 +0800

    style:测试代码

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,7 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="使用shell命令进行操作git,远程推送代码"/>
   <HelloWorld msg="使用shell命令进行操作git,远程推送代码"/>
-  <HelloWorld msg="使用shell命令进行操作git,远程推送代码"/>
-  <HelloWorld msg="使用shell命令进行操作git,远程推送代码"/>
-  <HelloWorld msg="使用shell命令进行操作git,远程推送代码"/>
+
 </template>
 
 <script lang="ts">
