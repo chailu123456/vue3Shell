74977883a61bf80e1b7c9f3cdd13d6fe6dea3722
chailu <chailu@rp-pet.cn>
2021-08-31 10:26:42 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,8 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="使用shell命令进行操作git,远程推送代码"/>
   <HelloWorld msg="使用shell命令进行操作git,远程推送代码"/>
-<<<<<<< HEAD
   <h3>{{a}}</h3>
   <button @click="btn">+1</button>
-=======
-  <HelloWorld msg="使用shell命令进行操作git,远程推送代码"/>
->>>>>>> 0f9b5642a03625a9060858830b968ce41c1b0db2
 
 
 
