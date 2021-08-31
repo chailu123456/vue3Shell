0f9b5642a03625a9060858830b968ce41c1b0db2
chailu <chailu@rp-pet.cn>
2021-08-31 10:05:21 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="使用shell命令进行操作git,远程推送代码"/>
-
   <HelloWorld msg="使用shell命令进行操作git,远程推送代码"/>
-  <h3>{{a}}</h3>
-  <button @click="btn">点击</button>
+  <HelloWorld msg="使用shell命令进行操作git,远程推送代码"/>
+
 
 
 </template>
 
-
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     HelloWorld
-  },
-  setup() {
-    const a = ref(1);
-    const btn = () => {
-      a.value++;
-    }
-
-    return {
-      btn,
-      a
-    }
   }
 });
 </script>
