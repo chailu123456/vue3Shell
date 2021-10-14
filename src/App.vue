4c4ee888284ff8b160617db6770b0ac604270da5
chailu <chailu@rp-pet.cn>
2021-10-14 19:21:00 +0800

    feat: update

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,20 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="使用shell命令进行操作git,远程推送代码"/>
 
-  <HelloWorld msg="测试1"/>
-  <HelloWorld msg="测试1"/>
-  <Test></Test>
-
   <button @click="btn">+1</button>
-
-
-
 
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
 import Test from './components/test.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld,
-    Test
+    // HelloWorld,
+    // Test
   }
 });
 </script>
